e60bdeccb52f39183f19b318471da1ef44057728
Neeraj-Pant01 <npant923@gmail.com>
2024-02-03 11:58:17 +0530

    updated 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Classmates from "./component/nav_pages/Classmates.jsx";
 import Feedback from "./component/nav_pages/feedback.jsx";
 import Old_paper from "./component/nav_pages/old_paper.jsx";
 import DirectorMessage from './pages/directorMessage/DirectorMessage.jsx';
+import About from './component/home/about.jsx';
+import Departmen from './pages/departmen/Departmen.jsx';
 
 function App() {
 
@@ -24,6 +26,8 @@ function App() {
             <Route path="/feedback" element={<Feedback/>}/>
             <Route path="/oldpaper" element={<Old_paper/>}/>
             <Route path='/director-message' element={<DirectorMessage />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/deparment' element={<Departmen />} />
         </Routes>
       </BrowserRouter>
   )
